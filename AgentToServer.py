cde51762c113a14d776da7257fe24afacde12e90
Aldrienne Janne G. Maniti <aldriennej@gmail.com>
2020-09-23 13:21:57 +0800

    Added design for AgentToServerComponent

--- AgentToServer.py
+++ AgentToServer.py
@@ -5,35 +5,55 @@ import winreg as winreg
 import socket
 
 class Window:
+
 	def __init__(self,master,server_ip,server_port,agent_port):
 		self.server_ip = server_ip
 		self.server_port = server_port
 		self.agent_port = agent_port
 
-		self.server_ip_label = tk.Label(text="Server IP Address")
-		self.server_ip_entry = tk.Entry()
-		self.server_port_label=tk.Label(text="Port")
+		#Frame for title
+		self.title_frame = tk.Frame(master=master,width=500,height=50, pady=5)
+		self.title_label = tk.Label(
+			master=self.title_frame,
+			fg='#d71920',
+			text="TEST CONNECTION",
+			font=('Helvetica neue',20,'bold'))
+		self.title_frame.pack(pady=15)
+		self.title_label.pack()
 
+		#Frame for Server IP
+		self.server_ip_frame = tk.Frame(master=master,width=400,height=50)
+		self.server_ip_label = tk.Label(master=self.server_ip_frame,text="Server IP Address")
+		self.server_ip_entry = tk.Entry(master=self.server_ip_frame)
+		self.server_ip_frame.pack(fill=tk.X,padx=80,pady=5)
+		self.server_ip_label.pack(side=tk.LEFT)
+		self.server_ip_entry.pack(side=tk.RIGHT)
+
+		#Frame for Port
+		self.port_frame = tk.Frame(master=master,width=400,height=50)
+		self.server_port_label=tk.Label(master=self.port_frame,text="Port")
+		self.server_port_entry = tk.Entry(master=self.port_frame)
+		self.port_frame.pack(fill=tk.X,padx=80,pady=10)
+		self.server_port_label.pack(side=tk.LEFT)
+		self.server_port_entry.pack(side=tk.RIGHT)
+		self.server_port_entry.insert(0, self.server_port)
+
+		#Frame for Butttons
+		self.list_buttons_frames = tk.Frame(master=master,width=400,height=50)
+		self.testButton = tk.Button(master=self.list_buttons_frames,text="Test Connection", command=self.testConnection,width=13)
+		self.testSmartScan = tk.Button(master=self.list_buttons_frames,text="Test SmartScan", command=self.testSmartScan, width=13)
+		self.testPorts_button = tk.Button(master=self.list_buttons_frames,text="Test Ports",command=self.testPorts, width=13)
+		self.list_buttons_frames.pack(pady=20)
+		self.testButton.pack()
+		self.testSmartScan.pack()
+		self.testPorts_button.pack()
+
+		#Frame for results
 		self.connection_status_label = tk.Label(text="Connection Status: ")
 		self.smartscan_status_label = tk.Label(text="SmartScan Status: ")
 		self.server_ports_label = tk.Label(text="Server Ports:")
 		self.agent_ports_label = tk.Label(text="Agent Ports")
-
-		self.server_port_entry = tk.Entry()
-		self.testSmartScan = tk.Button(text="Test SmartScan", command=self.testSmartScan)
-		self.testButton = tk.Button(text="Test Connection", command=self.testConnection)
-		self.testPorts_button = tk.Button(text="Test Ports",command=self.testPorts)
-
 		self.server_ip_entry.insert(0,self.server_ip)
-		self.server_port_entry.insert(0,self.server_port)
-
-		self.server_ip_label.pack()
-		self.server_ip_entry.pack()
-		self.server_port_label.pack()
-		self.server_port_entry.pack()
-		self.testButton.pack()
-		self.testSmartScan.pack()
-		self.testPorts_button.pack()
 		self.connection_status_label.pack()
 		self.smartscan_status_label.pack()
 		self.server_ports_label.pack()
@@ -47,8 +67,10 @@ class Window:
 			x = requests.get(url,verify=False)
 			if '-2' in x.text:
 				self.connection_status_label['text']='Connection Status:OK!'
+				self.connection_status_label['fg'] = '#5cb85c'
 		except:
 			self.connection_status_label['text']='Status:Unable to Reach Server'
+			self.connection_status_label['fg']='#d9534f'
 
 	def testSmartScan(self):
 		url = 'https://' + self.server_ip_entry.get() + ':' + self.server_port_entry.get() + '/tmcss/?LCRC=08000000AC41080092000080C4F01936B21D9104'
@@ -56,16 +78,18 @@ class Window:
 			x = requests.get(url,verify=False)
 			if x.headers['Content-Type'] == 'binary/octet-stream':
 				self.smartscan_status_label['text'] = 'SmartScan Status: OK!'
+				self.smartscan_status_label['fg'] = '#5cb85c'
 			else:
 				self.smartscan_status_label['text'] = 'SmartScan Status: Unable to Reach SmartScan Server'
+				self.smartscan_status_label['fg'] = '#d9534f'
 		except:
 			self.smartscan_status_label['text'] = 'SmartScan Status: Unable to Reach SmartScan Server'
+			self.smartscan_status_label['fg'] = '#d9534f'
 
 	def testPorts(self):
 		server_port_list = [80,8080,int(self.server_port_entry.get())]
 		closed_server_ports = ''
 		connect_server = True
-
 
 		try:
 			for port in server_port_list:
@@ -109,5 +133,5 @@ if __name__ == '__main__':
 	root = tk.Tk()
 	Window = Window(master=root,server_ip=initial_server,server_port=initial_port,agent_port=initial_agent_port)
 	root.title("Agent Test Connection")
-	root.geometry('500x600')
+	root.geometry('400x400')
 	root.mainloop()
